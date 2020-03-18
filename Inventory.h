81dd45a499218cc9e703ace35b6b170ea829a6ee
MoHass332 <62344175+MoHass332@users.noreply.github.com>
2020-03-18 14:56:06 +0000

    Create Inventory.h

---
+++ Inventory.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "iven.cpp"
+class inventory
+{
+public:
+    inventory();
+
+    std::vector <item*> Inventory;
+
+    unsigned int itemNumber;
+    unsigned int maxSize;
+    unsigned int numItemsHeld;
+
+    void addArmor( int itemClass, int itemID );
+    void addConsumable( int itemClass, int itemID );
+    void addItem( int itemType, int itemClass, int itemID );
+    void addQuestItem( int itemClass, int itemID );
+    void addWeapon( int itemClass, int itemID );
+    void removeItem( unsigned short int b );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
